1b7f3b388419a50c3534b7e83ce06faca647cad2
git-oberoi <shivamraj91150@gmail.com>
2026-07-29 04:51:15 +0530

    test-fixes

--- test-project/terraform/ec2.tf
+++ test-project/terraform/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "k3s_server" {
-  ami                    = "ami-0f58b397bc5c1f2e8" # Ubuntu 24.04 LTS (ap-south-1)
-  instance_type          = "t3.micro"
+  ami                    = "ami-01a00762f46d584a1" # Ubuntu 26.04 LTS (ap-south-1)
+  instance_type          = "t3.small"
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
   key_name               = "terraform-key"
 
   associate_public_ip_address = true
+
   tags = {
     Name = "${var.project_name}-k3s"
   }
